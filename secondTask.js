2b50a16c77dcd3fd8e6455e9396c72c8c217c057
darkit-prog <darya.kitaina@gmail.com>
2025-07-26 13:44:29 +0300

    fix after review

--- secondTask.js
+++ secondTask.js
@@ -1,8 +1,11 @@
 // Напишите скрипт, который принимает целое положительное число n (одно любое от 1 до 9), и выводит в консоль сумму равную n + nn + nnn, где n не преображаются, а конкатенируют
 function sumMultipleNumber(n) {
-    if (n >= 1 && n <= 9) {
-        const nn = Number(String(n) + String(n)); //55
-        const nnn = Number(String(n) + String(n) + String(n)); //555
+    if (typeof n !== 'number') {
+        console.log("n must be a number");
+    }
+    else if (n >= 1 && n <= 9) {
+        const nn = Number(String(n) + n); //55
+        const nnn = Number(String(n) + n + n); //55
         const sum = n + nn + nnn;
         console.log(sum);
     } else {
@@ -11,3 +14,8 @@ function sumMultipleNumber(n) {
 }
 
 sumMultipleNumber(5)
+sumMultipleNumber(55)
+sumMultipleNumber("5")
+sumMultipleNumber(true)
+sumMultipleNumber(null)
+sumMultipleNumber(undefined)
