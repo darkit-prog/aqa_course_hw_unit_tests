c57fa30f5b8bef170a70c74d790e098d07a6c5f7
darkit-prog <darya.kitaina@gmail.com>
2025-08-03 23:34:02 +0300

    move code from separate files in homework solution

--- homework-solutions/js/hw-3/task-1.js
+++ homework-solutions/js/hw-3/task-1.js
@@ -6,11 +6,24 @@
 
 3. Вывести в консоль item_1.
 
+*/
+
+const item_1 = 5;
+console.log(item_1);
+
+/*
 4. Создать переменную “item_2”
 
 5. Присвоить переменной item_2 цифру 3.
 
 6. Вывести в консоль item_2.
+
+*/
+
+const item_2 = 3;
+console.log(item_2);
+
+/*
 
 7. Создать переменную “item_3”
 
@@ -18,19 +31,46 @@
 
 9. Вывести в консоль item_3.
 
+*/
+
+const item_3 = item_1 + item_2;
+console.log(item_3);
+
+/*
+
 10. Создать переменную “item_4”
 
 11. Присвоить переменной item_4 строку "Hello World"
 
 12. Вывести в консоль item_4.
 
+*/ 
+
+const item_4 = "Hello World";
+console.log(item_4);
+
+/*
+
 13. Вывести в консоль сложение item_3 и item_4.
 
 14. Вывести в консоль умножение item_3 и item_4.
 
+*/
+
+console.log(item_3 + item_4);
+console.log(item_3 * item_4);
+
+/*
+
 15. Создать переменную “item_5”
 
 16. Присвоить переменной item_5 переменную item_3
+*/
+
+const item_5 = item_3;
+console.log(item_5);
+
+/*
 
 17. Создать переменную item_6.
 
@@ -42,6 +82,13 @@
 
 21. Вывести в консоль тип данных item_6 в виде ——  “item_6 == ”  item_6,  “item_6_type == ”  item_6_type ——  
 
+*/
+
+const item_6 = 15;
+const item_6_type = typeof item_6;
+console.log("-- item_6 == " + item_6, "item_6_type == " + item_6_type + " --");
+
+/*
 22. Создать переменную item_7 и в ней преобразовать item_6 в String.
 
 23. Создать переменную item_7_type
@@ -51,3 +98,7 @@
 25. Вывести в консоль тип данных item_7 в виде ——  “item_7 == ”  item_7,  “item_7_type == ”  item_7_type ——  
 
 */
+
+const item_7 = String(item_6);
+const item_7_type = typeof item_7;
+console.log("-- item_7 == " + item_7, "item_7_type == " + item_7_type + " --");
