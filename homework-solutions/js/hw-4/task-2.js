8afc8e0889eab673b2e263c76b532992b65de7af
darkit-prog <darya.kitaina@gmail.com>
2025-08-03 22:29:43 +0300

    Update task-2.js with new if for between checks

--- homework-solutions/js/hw-4/task-2.js
+++ homework-solutions/js/hw-4/task-2.js
@@ -23,7 +23,7 @@ if (typeof age !== "number") {
   console.log("Incorrect data type");
 } else if (age < minAge) {
   console.log("You don't have access cause your age is " + age + " It's less then " + minAge);
-} else if (age >= minAge && age <= maxAge) {
+} else if (age >= minAge && age < maxAge) {
   console.log("Welcome!");
 } else if (age > maxAge) {
   console.log( "Keep calm and look Culture channel");
